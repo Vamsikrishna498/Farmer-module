5fa20607de1a589780e7fc2e9ef8c751ddf96aaa
Vamsikrishna498 <vamsikrishna.m@hinfinity.in>
2025-06-13 08:34:12 +0530

    latest changes

--- target/generated-sources/annotations/com/farmer/farmermanagement/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/farmer/farmermanagement/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-12T14:49:14+0530",
+    date = "2025-06-13T08:26:49+0530",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.v20250514-1000, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
